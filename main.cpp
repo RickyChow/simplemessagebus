b34f47728b5e7940c31905f24dd14858b5c93b16
Ricky Chow <rickyc@nojapower.com.au>
2018-01-19 22:33:53 +1000

    added some more examples

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #include "messagebus.h"
-#include <set>
 
 EventMessageBus g_messageBus;
 
@@ -10,6 +9,13 @@ struct SignalData
   int id;
   std::string data;
 };
+
+struct SignData
+{
+  int id;
+  int magicNumber;
+};
+
 
 
 class SignalDataConsumer
@@ -25,7 +31,7 @@ public:
   void ReceivedEvent(SignalData& signalData)
   {
     if (signalData.id == m_id) {
-      printf("Received event data: %s!\r\n", signalData.data.c_str());
+      printf("SignalDataConsumer Received event data: %s!\r\n", signalData.data.c_str());
     }
   }
 
@@ -33,14 +39,31 @@ private:
   int m_id;
 };
 
-class EmptyClass
+class AllConsumer
+  : public EventSubscriber<SignalData>
+  , public EventSubscriber<SignData>
 {
-  
+public:
+  AllConsumer()
+  {
+    g_messageBus.Subscribe<SignalData>(this);
+    g_messageBus.Subscribe<SignData>(this);
+  }
+  void ReceivedEvent(SignalData& signalData)
+  {
+    printf("AllConsumer received signalData %s!\r\n", signalData.data.c_str());
+  }
+
+  void ReceivedEvent(SignData& signData)
+  {
+    printf("AllConsumer received signData magic number: %d!\r\n", signData.magicNumber);
+  }
 };
 
 int main()
 {
   SignalDataConsumer dataConsumer(1);
+  AllConsumer allConsumer;
 
   SignalData signalData1;
   signalData1.id = 1;
@@ -50,10 +73,12 @@ int main()
   signalData2.id = 2;
   signalData2.data = "Data for signal 2";
 
-  EmptyClass test;
+  SignData signData1;
+  signData1.id = 3;
+  signData1.magicNumber = 23;
 
   g_messageBus.Publish(signalData1);
   g_messageBus.Publish(signalData2);
-  g_messageBus.Publish(test);
+  g_messageBus.Publish(signData1);
   return 0;
 }
--- messagebus.h
+++ messagebus.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <map>
+#include <set>
 //////////////////////////////////////////////////////////////////
 
 //This is used so that we have a common form of storing subscribers
@@ -31,27 +32,41 @@ private:
     std::string eventId = typeid(data_t).name();
     return eventId;
   }
+  typedef std::set<EventSubscriberGeneric*> subscribercol_t;
+  std::map<std::string, subscribercol_t> m_subscribers;
 
 public:
   template<typename data_t>
   void Publish(data_t& data)
   {
-    EventSubscriberGeneric* sub = m_subscribers[GetEventId<data_t>()];
+    subscribercol_t subs = m_subscribers[GetEventId<data_t>()];
 
-    EventSubscriber<data_t>* subTyped = dynamic_cast<EventSubscriber<data_t>* >(sub);
-    data_t* dataTyped = dynamic_cast<data_t*>(&data);
-    if (!dataTyped || !subTyped) return;
-    subTyped->ReceivedEvent(*dataTyped);
+    subscribercol_t::iterator sub = subs.begin();
+
+    for (; sub != subs.end(); ++sub) {
+
+      EventSubscriber<data_t>* subTyped = 
+        dynamic_cast<EventSubscriber<data_t>* >(*sub);
+      data_t* dataTyped = dynamic_cast<data_t*>(&data);
+
+      if (dataTyped && subTyped) {
+        subTyped->ReceivedEvent(*dataTyped);
+      }
+    }
   }
 
   template<typename data_t>
   void Subscribe(EventSubscriber<data_t>* subscriber)
   {
-    m_subscribers[GetEventId<data_t>()] = subscriber; 
+    m_subscribers[GetEventId<data_t>()].insert(subscriber); 
   }
 
-private:
-  std::map<std::string, EventSubscriberGeneric*> m_subscribers;
+  template<typename data_t>
+  void Unsubscribe(EventSubscriber<data_t>* subscriber)
+  {
+    m_subscribers[GetEventId<data_t>()].erase(subscriber); 
+  }
+
 };
 
 
